2ce14057604a273cc1654b75be55db801b157e88
Angel Blackcoat <132302338+angelblackcoat@users.noreply.github.com>
2026-06-05 09:40:13 +0700

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: '/my-repo',
   integrations: [
     tailwind({
       applyBaseStyles: false,
